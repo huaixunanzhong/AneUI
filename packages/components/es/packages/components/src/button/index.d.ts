8a0c4f5cf5590a70b1e4cf09ec5e80d5ab263d60
chenguolong <1582576318@qq.com>
2025-05-10 22:59:18 +0800

    feat: 提交git忽略文件以及未进行版本管理的文件

---
+++ packages/components/es/packages/components/src/button/index.d.ts
@@ -0,0 +1,4 @@
+import { Plugin } from 'vue';
+type SFCWithInstall<T> = T & Plugin;
+declare const Button: SFCWithInstall<import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, HTMLButtonElement>>;
+export default Button;
